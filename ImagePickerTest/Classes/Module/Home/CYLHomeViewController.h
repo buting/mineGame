1159a521df1c94d54d61eca88ef5df0206e06d49
buting <1917484@qq.com>
2016-05-24 00:07:58 +0800

    modifi

--- ImagePickerTest/Classes/Module/Home/CYLHomeViewController.h
+++ ImagePickerTest/Classes/Module/Home/CYLHomeViewController.h
@@ -4,7 +4,7 @@
 //
 //  Created by 微博@iOS程序犭袁 ( http://weibo.com/luohanchenyilong/ ) on 10/20/15.
 //  Copyright © 2015 https://github.com/ChenYilong . All rights reserved.
-//
+// test
 
 #import <UIKit/UIKit.h>
 
